0e5b56649ebb316c4dc9fe2735fa5b033e4e407f
mohit88512 <98802810+mohit88512@users.noreply.github.com>
2025-08-28 13:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let main = document.querySelectorAll("li");
+for(let i=0;i<main.length;i++){
+	if(main[i].id == 'level'){
+		alert(`The level of the element is: ${i}`)
+	}
+}
